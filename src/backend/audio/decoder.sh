e7197adfaad0ef895678da144410e4b93b87bfb9
FRD <fred@linkeo.com>
2026-04-27 12:27:52 +0200

    -af "aresample=async=1"

--- src/backend/audio/decoder.sh
+++ src/backend/audio/decoder.sh
@@ -13,7 +13,7 @@ until [ -p "$FIFO" ]; do sleep 1; done
 while true; do
   if curl -s -f http://127.0.0.1:8111/status-json.xsl | grep -q '"source"'; then
     ffmpeg -hide_banner -loglevel error -fflags nobuffer -flags low_delay -rw_timeout 5000000 \
-        -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 \
+        -reconnect 1 -af "aresample=async=1" -reconnect_streamed 1 -reconnect_delay_max 5 \
         -i http://127.0.0.1:8111/live \
         -f s16le -ar 48000 -ac 2 pipe:1 > "$FIFO" 2>/dev/null
     sleep 2
